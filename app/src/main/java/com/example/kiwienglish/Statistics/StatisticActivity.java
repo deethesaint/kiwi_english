b9248ba7df6563d346524519493f8eeeaf3a99eb
deethesaint <vtduong0912@gmail.com>
2023-05-29 10:27:43 +0700

    minor changes

--- app/src/main/java/com/example/kiwienglish/Statistics/StatisticActivity.java
+++ app/src/main/java/com/example/kiwienglish/Statistics/StatisticActivity.java
@@ -2,6 +2,7 @@ package com.example.kiwienglish.Statistics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ public class StatisticActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
 
+        TextView coursesRegisteredTextView = findViewById(R.id.stats_atvt_courses_registered_tv);
         TextView coursesAttendedTextView = findViewById(R.id.stats_atvt_courses_attended_tv);
         TextView correctAnswersTextView = findViewById(R.id.stats_atvt_correct_answer_tv);
         TextView wrongAnswersTextView = findViewById(R.id.stats_atvt_wrong_answer_tv);
@@ -29,6 +31,7 @@ public class StatisticActivity extends AppCompatActivity {
                 .equalTo("accountID", CurrentAccount.getInstance().getAccountID())
                 .findFirst();
         if (account != null) {
+            coursesRegisteredTextView.setText(String.valueOf(account.getCourseModels().size()));
             coursesAttendedTextView.setText(coursesAttendedTextView.getText().toString() + String.valueOf(account.getCoursesAttended()));
             correctAnswersTextView.setText(correctAnswersTextView.getText().toString() + String.valueOf(account.getCorrectAnswers()));
             wrongAnswersTextView.setText(wrongAnswersTextView.getText().toString() + String.valueOf(account.getWrongAnswers()));
